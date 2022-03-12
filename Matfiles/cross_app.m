9278d9aa766c8bd241bc247c4aefc74e8f0d084c
kandapva <kandan222@gmail.com>
2022-03-12 10:16:40 +0530

    ACA,CA codes edited

--- Matfiles/cross_app.m
+++ Matfiles/cross_app.m
@@ -1,4 +1,9 @@
 function [U,V]=cross_app(A,tol)
+%   This function computes the low rank approximaiton using the cross
+%   approximation technique.
+%   A -  size mxn 
+%   U,V' - size mxr 
+%   tol - default to pow(10,-10)
 error =1;
 iter=1;
 M=A;
@@ -14,6 +19,7 @@ a=A(:,js)/del;
 U(:,iter)=a;
 b=A(is,:);
 V(iter,:)=b;
+% The following three lines is what makes the algorithm scale as O(rmn)
 M=M-a*b;
 A=A-a*b
 error=norm(M);
@@ -23,6 +29,7 @@ if iter>8
 end
 end
 end
+% Routine that seeks the index of the maximum element in the matrix
 function [ a,b ] = maxmat( c )
 as=size(c);
 total_ele=numel(c);
--- Matfiles/partial_ACA.m
+++ Matfiles/partial_ACA.m
@@ -1,13 +1,19 @@
 function [U,V]=partial_ACA(A,mu)
+%   This routine computes low rank decomposition of A \approx UV
+%   A -  size mxn 
+%   U,V' - size mxr 
+%   mu - default to pow(10,-10)
 if ~exist('mu','var')
     mu=1e-10;
 end
 k=1;
 [m,n]=size(A);
+% i,j holds the index of row and column that has max pivot
 i=zeros(m,1);
 j=zeros(n,1);
 i(k)=1;Z=[];
 vq=A(i(k),:);
+% Initialize U,Vrank 1 approximation 
 while norm(vq)==0 && k<m
     Z(end+1)=i(k);
     k=k+1;
@@ -22,6 +28,8 @@ if k<m
 end
 k=k+1;
 err=norm(U(:,k-1))*norm(V(k-1,:));
+% The general routine that constructs rank 1 approximation at each
+% iteration
 while err>mu && k<m
    vq=zeros(size(vq));
    i(k)=find_max(U(:,k-1),Z);
@@ -29,6 +37,8 @@ while err>mu && k<m
         vq=vq-U(i(k),l)*V(l,:);
    end
    vq=vq+A(i(k),:);
+   % The err computed at each iteration is O(n) in opposed to O(n^2) in
+   % Cross Approximation
  if norm(vq)~=0
     Z(end+1)=i(k);
     j(k)=find_max(vq);
@@ -46,7 +56,7 @@ while err>mu && k<m
  end
 end
 end
-
+% Finds the index of the maximum element in the given vector
 function [maxi]=find_max(u,z)
 u=abs(u);
 ind=find(u==max(u));
